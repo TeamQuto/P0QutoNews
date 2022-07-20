06d65382fc58ceacf81db840d2dc55b9a7999db8
Vishal Mehta <103929700+EightFlix@users.noreply.github.com>
2022-07-20 21:31:51 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -26,7 +26,7 @@ async def answer(bot, query):
     if not await inline_users(query):
         await query.answer(results=[],
                            cache_time=0,
-                           switch_pm_text='okDa',
+                           switch_pm_text='Type Movies Name And Search',
                            switch_pm_parameter="hehe")
         return
 
